0ea2a91b1bdc06f40d75c8860e67e5391fc1f4bd
shafin90 <mashrafiahnam1@gmail.com>
2023-09-30 14:40:12 -0700

    push

--- food_fest_client/src/components/Footer/Footer.jsx
+++ food_fest_client/src/components/Footer/Footer.jsx
@@ -1,25 +1,54 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
-import './Footer.css'
+import { FaFacebook, FaTwitter, FaInstagram, FaPinterest } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="bg-dark text-light py-4">
-      <Container>
+    <footer className="bg-dark text-light py-5">
+      <Container className="py-5">
         <Row>
-          <Col md={4} sm={6}>
-            <h5>Contact Us</h5>
-            <p>Email: example@example.com</p>
-            <p>Phone: (123) 456-7890</p>
+          <Col md={4}>
+            <h4>Contact Us</h4>
+            <p>
+              Address: 123 Main St<br />
+              City, State 12345<br />
+              Email: info@example.com<br />
+              Phone: (123) 456-7890
+            </p>
           </Col>
-          <Col md={4} sm={6}>
-            <h5>Follow Us</h5>
-            <p>Facebook</p>
-            <p>Twitter</p>
-            <p>Instagram</p>
+
+
+          <Col md={4}>
+            <h4>Follow Us</h4>
+
+            <FaFacebook className=' me-2 ' />
+
+
+
+            <FaTwitter className=' me-2 ' />
+
+            <FaInstagram className=' me-2 ' />
+
+            <FaPinterest />
+
           </Col>
-          <Col md={4} className="d-md-flex justify-content-md-end">
-            <p>&copy; {new Date().getFullYear()} Your Company</p>
+
+
+          <Col md={4}>
+            <h4>Newsletter</h4>
+            <p>Subscribe to our newsletter for the latest updates and recipes.</p>
+            <form>
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter your email"
+                />
+              </div>
+              <button type="submit" className="btn btn-primary mt-3">
+                Subscribe
+              </button>
+            </form>
           </Col>
         </Row>
       </Container>
